0fb3ef3090e9fd1ae34a178ea6e87982f7c6187f
aidenpaulliu <aidenpaulliu@gmail.com>
2026-05-26 19:18:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ function buildCoverHtml(backgroundImage, hookText, brandHandle) {
       position: absolute;
       bottom: 180px;
       left: 64px;
-      width: 850px;
+      width: 900px;
       display: flex;
       flex-direction: column;
       align-items: flex-start;
